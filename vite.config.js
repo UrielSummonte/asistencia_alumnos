4a45d9c8069846bc0e372649ec2c31fa8906d8ba
Uriel <seti137@gmail.com>
2026-04-01 10:58:31 -0300

    incluye vitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nombre de mi App',
+        short_name: 'MiApp',
+        description: 'Mi increíble aplicación en Vercel',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // Asegúrate de tener estas imágenes en tu carpeta /public
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
